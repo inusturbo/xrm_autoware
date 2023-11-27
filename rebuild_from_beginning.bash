589043d6ea0e98ddced0388ab6e12ebe1b945a95
MA Shanpeng <shanpeng.ma@gmail.com>
2023-11-27 15:54:10 +0900

    Update rebuild_from_beginning.bash

--- rebuild_from_beginning.bash
+++ rebuild_from_beginning.bash
@@ -14,4 +14,11 @@ vcs import src < calibration_tools.repos
 
 source /opt/ros/humble/setup.bash
 
+cd
+cd autoware/src/sensor_component/external/xrm_rslidar_sdk
+git submodule init
+git submodule update
+sudo apt-get install -y libyaml-cpp-dev
+sudo apt-get install -y  libpcap-dev
+
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
